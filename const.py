c1f3e8e85248a81e49d9e116bbcec7a7d535f375
Radioactiverm <76690913+Radioactiverm@users.noreply.github.com>
2025-11-30 01:10:56 +0100

    Create const.py

---
+++ const.py
@@ -0,0 +1,12 @@
+"""Constants for the Donut SMP integration."""
+
+DOMAIN = "donutsmp"
+CONF_USERNAME = "username"
+CONF_API_KEY = "api_key"
+
+# Base URLs
+API_STATS_URL = "https://api.donutsmp.net/v1/stats/{}"
+API_LOOKUP_URL = "https://api.donutsmp.net/v1/lookup/{}"
+
+# Update interval
+UPDATE_INTERVAL_SECONDS = 60
